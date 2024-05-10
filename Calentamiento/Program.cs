8a7944b10aec7314687de89edce7195c78777b91
obifg <obifg@DESKTOP-2ON52NU>
2024-05-09 23:47:43 -0400

    Agregar archivos de proyecto.

---
+++ Calentamiento/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calentamiento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa que calcula el área de un rectángulo
+            double ladoA, ladoB, resultado;
+            Console.WriteLine("Calcula el area de un rectangulo");
+            Console.Write("Ingrese el valor del lado A: ");
+
+            ladoA = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese el valor del lado B: ");
+            ladoB = Convert.ToDouble(Console.ReadLine());
+
+            resultado = ladoA * ladoB;
+
+            Console.WriteLine("El área del rectángulo es: " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
